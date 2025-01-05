78d70d71fa053c0514d5b8b389fbc23199a3beb0
Evgene Shlyk <serpupstars@gmail.com>
2025-01-06 07:20:52 +1000

    понедельник 6 января

--- src/components/study/study.tsx
+++ src/components/study/study.tsx
@@ -1,14 +1,5 @@
-// import {style} from './study.module.scss'
-import styles from './study.module.scss'
+import styles from './study.module.scss';
 
-export default function Study() {
-  return (
-    <div>
-      <div className={styles.study}>Hello world</div>
-      <div className={styles.study__output}></div>
-    </div>
-  );
 
-}
 
 
