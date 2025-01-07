4e9caba5b46bc1c44d0485df91ab0a232d18676f
Evgene Shlyk <serpupstars@gmail.com>
2025-01-08 06:58:14 +1000

    среда 8 января

--- src/components/study/box.tsx
+++ src/components/study/box.tsx
@@ -1,20 +1,60 @@
 import styles from './list.module.scss';
 
+// function Item({product}) {
+//   return (
+//     <li className={styles.list__item}>
+//       <span>{product.name}</span>
+//       <span>{product.price}</span>
+//     </li>
+//   )
+// }
+//
+// function List({children}) {
+//   return (
+//     <ul>
+//       {children}
+//     </ul>
+//   )
+// }
+//
+// export default function Box({products}) {
+//   const sortedProducts = products.reduce((acc, product) => {
+//     if (!acc[product.category]) {
+//       acc[product.category] = [];
+//     }
+//     acc[product.category].push(product);
+//     return acc;
+//   }, {})
+//
+//   return (
+//     <div>
+//       {Object.keys(sortedProducts).map(category => (
+//         <div key={category}>
+//           <h2 className={styles.list__title}>{category}</h2>
+//           <List>
+//             {sortedProducts[category].map(product =>
+//               <Item key={product.name} product={product}/>
+//             )}
+//           </List>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
 function Item({product}) {
   return (
     <li className={styles.list__item}>
       <span>{product.name}</span>
       <span>{product.price}</span>
     </li>
-  )
+  );
 }
 
 function List({children}) {
   return (
-    <ul>
-      {children}
-    </ul>
-  )
+    <ul>{children}</ul>
+  );
 }
 
 export default function Box({products}) {
@@ -24,7 +64,7 @@ export default function Box({products}) {
     }
     acc[product.category].push(product);
     return acc;
-  }, {})
+  }, {});
 
   return (
     <div>
@@ -39,7 +79,7 @@ export default function Box({products}) {
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 
