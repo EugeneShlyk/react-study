50eb22bd6ff3a1cfaa359e1880d13d875fc8c2b4
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2025-01-08 17:31:05 +1000

    всввссссс

--- src/components/study/box.tsx
+++ src/components/study/box.tsx
@@ -1,20 +1,19 @@
 import styles from './list.module.scss';
 
+
 // function Item({product}) {
 //   return (
 //     <li className={styles.list__item}>
 //       <span>{product.name}</span>
 //       <span>{product.price}</span>
 //     </li>
-//   )
+//   );
 // }
 //
 // function List({children}) {
 //   return (
-//     <ul>
-//       {children}
-//     </ul>
-//   )
+//     <ul>{children}</ul>
+//   );
 // }
 //
 // export default function Box({products}) {
@@ -24,7 +23,7 @@ import styles from './list.module.scss';
 //     }
 //     acc[product.category].push(product);
 //     return acc;
-//   }, {})
+//   }, {});
 //
 //   return (
 //     <div>
@@ -39,7 +38,7 @@ import styles from './list.module.scss';
 //         </div>
 //       ))}
 //     </div>
-//   )
+//   );
 // }
 
 function Item({product}) {
@@ -48,38 +47,36 @@ function Item({product}) {
       <span>{product.name}</span>
       <span>{product.price}</span>
     </li>
-  );
+  )
 }
 
 function List({children}) {
   return (
     <ul>{children}</ul>
-  );
+  )
 }
 
 export default function Box({products}) {
   const sortedProducts = products.reduce((acc, product) => {
     if (!acc[product.category]) {
-      acc[product.category] = [];
+      acc[product.category] = []
     }
-    acc[product.category].push(product);
+    acc[product.category].push(product)
     return acc;
-  }, {});
+  }, {})
 
   return (
-    <div>
+    <div className={styles.box}>
       {Object.keys(sortedProducts).map(category => (
-        <div key={category}>
+        <>
           <h2 className={styles.list__title}>{category}</h2>
           <List>
             {sortedProducts[category].map(product =>
-              <Item key={product.name} product={product}/>
+              <Item product={product}></Item>
             )}
           </List>
-        </div>
+        </>
       ))}
     </div>
-  );
+  )
 }
-
-
