2af277d604bbdf1c6fd02e46e728c93102077d23
Eugene Shlyk <serpupstars@gmail.com>
2025-02-21 07:22:14 +1000

    house 7 -19

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,13 +1,28 @@
 import styles from './map.module.scss';
 import {useRef} from "react";
+import useMap from "../../hoooks/useMap.tsx";
 
-export default function () {
+type CityProp = {
+  title: string;
+  lat: number;
+  lng: number;
+  zoom: number;
+};
+
+interface MapProps {
+  city: CityProp; // Определяем, что city - это объект типа CityProp
+}
+
+
+export default function ({city}: MapProps) {
   const mapRef = useRef<HTMLDivElement>(null);
+  const map = useMap(mapRef, city);
 
   return (
     <div
       className={styles.map}
       style={{height: '500px'}}
+      ref={mapRef}
     >
     </div>
   );
--- src/hoooks/useMap.tsx
+++ src/hoooks/useMap.tsx
@@ -7,17 +7,37 @@ type cityProp = {
   zoom: number,
 };
 
-export default function useMap(mapRef: Ref<HTMLDivElement>, city: cityProp) {
+function useMap(mapRef: Ref<HTMLDivElement>, city: cityProp) {
   const [map, setMap] = useState(null);
   const isRenderedRef = useRef<HTMLDivElement>(false);
 
   useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = leaflet.map(mapRef.current, {
+        center: {
+          lat: city.lat,
+          lng: city.lng,
+        },
+        zoom: city.zoom,
+      });
 
-  }, [])
+      leaflet
+        .tileLayer(
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+          {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          },
+        )
+        .addTo(instance);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
 
   return map;
 }
 
-
+export default useMap;
 
 
